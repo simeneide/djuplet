16e7a3c564ee7b860be4fce5a39a8754a4ba78c5
simeneide <simeneide>
2025-02-08 11:39:08 +0000

    the num_procceses argument seems to not work

--- ds_train/ds_train/run.sh
+++ ds_train/ds_train/run.sh
@@ -1,1 +1,1 @@
-accelerate launch run_r1_grpo.py --num_processes 1  --config grpo-qwen-2.5-3b-deepseek-r1-countdown.yaml
+accelerate launch run_r1_grpo.py --config grpo-qwen-2.5-3b-deepseek-r1-countdown.yaml
