7647288e70af00a61d5df2d5dfd8e52faf594fc0
simeneide <simeneide>
2025-02-09 09:13:01 +0000

    add binary reward function that only gives point if wer is almost zero

--- ds_train/ds_train/run_r1_grpo.py
+++ ds_train/ds_train/run_r1_grpo.py
@@ -181,6 +181,53 @@ def corrupt_reward_func(completions, original_text, **kwargs):
             rewards.append(0.0) 
     return rewards
 
+def corrupt_reward_binary_func(completions, original_text, **kwargs):
+    """
+    Evaluates completions based on:
+    2. Mathematical correctness of the answer
+
+    Args:
+        completions (list[str]): Generated outputs
+        target (list[str]): Expected answers
+        nums (list[str]): Available numbers
+    
+    Returns:
+        list[float]: Reward scores
+    """
+    rewards = []
+    for completion, ground_truth in zip(completions, original_text):
+      try:
+        # add synthetic <think> as its already part of the prompt and prefilled for the assistant to more easily match the regex
+        completion = "<think>" + completion
+        # Check if the format is correct
+        match = re.search(r"<answer>(.*?)<\/answer>", completion)
+        if match is None:
+            rewards.append(0.0)
+            continue
+        # Extract the "answer" part from the completion
+        generated_answer = match.group(1).strip()
+        # Extract all numbers from the equation
+        error_rate = wer(generated_answer, ground_truth)
+        score = min(1.0, max(0.0, 1- error_rate))
+        
+        # Check if the equation is correct and matches the ground truth
+        if score>=0.98:
+            rewards.append(1.0)
+            if random.random() < 0.10:  # 10% chance to write fully successful samples into a file
+                os.makedirs("completion_samples", exist_ok=True)
+                log_file = os.path.join("completion_samples", "success_completion_samples_binary.txt")
+                with open(log_file, "a") as f:
+                    f.write(f"\n\n==============\n")
+                    f.write(completion)
+        else:
+            rewards.append(0.0)
+
+      except Exception as e:
+            print(e)
+            # If evaluation fails, reward is 0
+            rewards.append(0.0) 
+    return rewards
+
 
 def get_checkpoint(training_args: GRPOConfig):
     last_checkpoint = None
@@ -250,7 +297,7 @@ def grpo_function(
     trainer = GRPOTrainer(
       model=model_args.model_name_or_path,
       processing_class=tokenizer,
-      reward_funcs=[format_reward_func, corrupt_reward_func],
+      reward_funcs=[format_reward_func, corrupt_reward_func, corrupt_reward_binary_func],
       args=training_args,
       train_dataset=dataset['train'],
       eval_dataset=dataset['validation'],
